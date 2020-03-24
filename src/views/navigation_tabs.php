0159b00365aff4f71e3a77371271fbb7e47e7d4f
Pozitronik <antikillerxxl@gmail.com>
2020-03-24 15:00:23 +0300

    support 'visible' option in tab items

--- src/views/navigation_tabs.php
+++ src/views/navigation_tabs.php
@@ -15,8 +15,10 @@ use yii\web\View;
 
 <ul class="nav nav-tabs">
 	<?php foreach ($items as $item): ?>
-		<?php $sameUrl = Utils::isSameUrlPath($item['url']); ?>
-		<?= Html::tag('li', Html::a($item['label'], $sameUrl?'#':$item['url'], ArrayHelper::getValue($item, 'linkOptions', [])), ['class' => $sameUrl?'active':'inactive']) ?>
+		<?php if (true === ArrayHelper::getValue($item, 'visible', true)): ?>
+			<?php $sameUrl = Utils::isSameUrlPath($item['url']); ?>
+			<?= Html::tag('li', Html::a($item['label'], $sameUrl?'#':$item['url'], ArrayHelper::getValue($item, 'linkOptions', [])), ['class' => $sameUrl?'active':'inactive']) ?>
+		<?php endif; ?>
 	<?php endforeach; ?>
 </ul>
 
